bab66208a0814400ec8db9da6563927970fa74cc
xiaojun996 <blueskyzx@icloud.com>
2021-06-17 10:34:07 +0800

    feat: 随机背景～适配dark

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,7 @@
 const self = globalThis || window
 const canvas = document.querySelector('#canvas')
 const rect = canvas.getBoundingClientRect()
+const body = document.body.getBoundingClientRect()
 canvas.width = rect.width
 canvas.height = rect.height
 
@@ -11,12 +12,50 @@ canvas.height = rect.height
  */
 const { RaindropFX } = self
 
-const dark = 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/751262.png'
-const light = 'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/843699.png'
+const { height, width } = body
+
+const darkDesktopImgList = [
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/751262.png',
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/917480.png',
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/999332.png',
+]
+const lightDesktopImgList = [
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/843699.png',
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/fate/862789.png',
+]
+
+const darkImgList = ['https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/twitter/3.jpeg']
+const lightImgList = [
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/twitter/2.jpeg',
+  'https://cdn.jsdelivr.net/gh/xiaojun996/CDN/images/anime/twitter/1.jpeg',
+]
+
+let dark = undefined
+let light = undefined
+
+const generatorRandom = num => {
+  return Math.floor(Math.random() * num)
+}
+
+if (height > width) {
+  // mobile
+  dark = darkImgList[generatorRandom(darkImgList.length)]
+  light = lightImgList[generatorRandom(lightImgList.length)]
+} else {
+  // desktop
+  dark = darkDesktopImgList[generatorRandom(darkDesktopImgList.length)]
+  light = lightDesktopImgList[generatorRandom(lightDesktopImgList.length)]
+}
 
 const option = {
   canvas: canvas,
   background: light,
+}
+
+if (self.matchMedia && self.matchMedia('(prefers-color-scheme: dark)').matches) {
+  option.background = dark
+} else {
+  option.background = light
 }
 
 const raindropFx = new RaindropFX(option)
