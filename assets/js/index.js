cc3a0c758e8e604d9b9914479af567b329c7028c
zhixiangyao <blueskyzx@icloud.com>
2024-08-09 15:26:46 +0800

    feat: 改进代码

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,58 @@
 'use strict'
 
-const canvas = document.querySelector('#canvas')
-const rect = canvas.getBoundingClientRect()
-const genRandom = num => Math.floor(Math.random() * num)
-
-const { origin } = location
-const prefixPC = `${origin}/assets/images/desktop/`
-const prefix = `${origin}/assets/images/mobile/`
-
-const lightsPC = ['1.png', '2.png']
-const darksPC = ['3.png', '4.png', '5.png', '6.jpeg', '7.jpeg']
-const darks = ['3.jpeg', '6.jpeg', '7.jpeg']
-const lights = ['2.jpeg', '1.jpeg', '4.jpeg', '5.jpeg']
-
-let dark = prefixPC + darksPC[genRandom(darksPC.length)]
-let light = prefixPC + lightsPC[genRandom(lightsPC.length)]
-
-const { height, width } = document.body.getBoundingClientRect()
-
-if (height > width) {
-  dark = prefix + darks[genRandom(darks.length)]
-  light = prefix + lights[genRandom(lights.length)]
-}
-
-canvas.width = rect.width
-canvas.height = rect.height
-
-const query = '(prefers-color-scheme: dark)'
-const option = { canvas, background: self.matchMedia && self.matchMedia(query).matches ? dark : light }
-const raindropFx = new window.RaindropFX(option)
-
-self.onresize = () => {
+/**
+ *
+ * @param {HTMLCanvasElement} canvas
+ */
+const init = canvas => {
   const rect = canvas.getBoundingClientRect()
-  raindropFx.resize(rect.width, rect.height)
+  const genRandom = num => Math.floor(Math.random() * num)
+
+  const { origin } = location
+  const prefixPC = `${origin}/assets/images/desktop/`
+  const prefix = `${origin}/assets/images/mobile/`
+
+  const lightsPC = ['1.png', '2.png']
+  const darksPC = ['3.png', '4.png', '5.png', '6.jpeg', '7.jpeg']
+  const darks = ['3.jpeg', '6.jpeg', '7.jpeg']
+  const lights = ['2.jpeg', '1.jpeg', '4.jpeg', '5.jpeg']
+
+  let dark = prefixPC + darksPC[genRandom(darksPC.length)]
+  let light = prefixPC + lightsPC[genRandom(lightsPC.length)]
+
+  const { height, width } = document.body.getBoundingClientRect()
+
+  if (height > width) {
+    dark = prefix + darks[genRandom(darks.length)]
+    light = prefix + lights[genRandom(lights.length)]
+  }
+
+  canvas.width = rect.width
+  canvas.height = rect.height
+
+  const query = '(prefers-color-scheme: dark)'
+  const option = { canvas, background: self.matchMedia && self.matchMedia(query).matches ? dark : light }
+  const raindropFx = new window.RaindropFX(option)
+
+  self.onresize = () => {
+    const rect = canvas.getBoundingClientRect()
+    raindropFx.resize(rect.width, rect.height)
+  }
+
+  self.matchMedia(query).addEventListener('change', e => {
+    const scheme = e.matches ? 'dark' : 'light'
+    if (scheme === 'dark') {
+      raindropFx.setBackground(dark)
+    } else if (scheme === 'light') {
+      raindropFx.setBackground(light)
+    } else {
+      raindropFx.setBackground(light)
+    }
+  })
+
+  raindropFx.start()
 }
 
-self.matchMedia(query).addEventListener('change', e => {
-  const scheme = e.matches ? 'dark' : 'light'
-  if (scheme === 'dark') {
-    raindropFx.setBackground(dark)
-  } else if (scheme === 'light') {
-    raindropFx.setBackground(light)
-  } else {
-    raindropFx.setBackground(light)
-  }
-})
+const canvas = document.querySelector('#canvas')
 
-raindropFx.start()
+canvas && init(canvas)
